7170aeda6b83892a94c8d83b0e1e507f8ff62f57
Roy Lulavy <49700286+roeilulavy@users.noreply.github.com>
2021-10-11 13:37:39 +0300

    hotfix: corrections2

--- scripts/index.js
+++ scripts/index.js
@@ -4,15 +4,20 @@ const profileAbout = document.querySelector('.profile__value-about');
 const profileEditButton = document.querySelector('.profile__button-edit');
 
 const popup = document.querySelector('.popup');
+const popupForm = popup.querySelector('.popup__form');
 const popupCloseButton = popup.querySelector('.popup__close');
-const popupSubmitButton = popup.querySelector('.popup__submit');
-const popupInputName = popup.querySelector('.popup__input_type_name');
-const popupInputAbout = popup.querySelector('.popup__input_type_about');
+const popupSubmitButton = popupForm.querySelector('.popup__submit');
+const popupInputName = popupForm.querySelector('.popup__input_type_name');
+const popupInputAbout = popupForm.querySelector('.popup__input_type_about');
 
 function openPopup() {
     popupInputName.value = profileName.textContent;
     popupInputAbout.value = profileAbout.textContent;
     popup.classList.add('popup_is-open');
+}
+
+function closePopup() {
+    popup.classList.remove('popup_is-open');
 }
 
 function saveUserInfo(event) {
@@ -22,12 +27,8 @@ function saveUserInfo(event) {
     closePopup();
 }
 
-function closePopup() {
-    popup.classList.remove('popup_is-open');
-}
-
 profileEditButton.addEventListener('click', openPopup);
 
-popup.addEventListener('submit', saveUserInfo);
-
 popupCloseButton.addEventListener('click', closePopup);
+
+popupForm.addEventListener('submit', saveUserInfo);
