5ebba1fa0b90abc9075c356b488872ed17a4edec
roeilulavy <roei19931@gmail.com>
2021-11-23 21:51:14 +0200

    add Section and Popup js

---
+++ scripts/Popup.js
@@ -0,0 +1,25 @@
+export default class Card {
+    constructor(popupSelector) {
+        this._popupSelector = document.querySelector(popupSelector);
+    }
+
+    _handleOutsideClose() {
+        
+    }
+
+    _handleEscClose() {
+
+    }
+
+    open() {
+
+    }
+
+    close() {
+
+    }
+
+    setEventListeners() {
+
+    }
+}
---
+++ scripts/Section.js
@@ -0,0 +1,23 @@
+export default class Section {
+    constructor({ data, renderer }, containerSelector) {
+      this._renderedItems = data;
+      this._renderer = renderer;
+      this._container = document.querySelector(containerSelector);
+    }
+  
+    addItem(element) {
+      this._container.append(element);
+    }
+  
+    clear() {
+      this._container.innerHTML = "";
+    }
+  
+    renderer() {
+      this.clear();
+  
+      this._renderedItems.forEach(item => {
+        this._renderer(item);
+      });
+    }
+  }
