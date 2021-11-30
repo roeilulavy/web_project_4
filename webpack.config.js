76367690874bc586e8bedc2f3bc5073f60cdc55f
roeilulavy <roei19931@gmail.com>
2021-11-30 21:57:41 +0200

    postcss Fix

--- webpack.config.js
+++ webpack.config.js
@@ -34,8 +34,12 @@ module.exports = {
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: "css-loader"
+            loader: "css-loader",
+            options: {
+              importLoaders: 1
+            }
           },
+          "postcss-loader"
         ],
       },
       {
